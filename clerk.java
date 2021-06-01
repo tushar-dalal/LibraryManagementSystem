a3a91532ec96204a838ba0f3030397f8472ed1ca
Tushar Dalal <59857119+tushar-dalal@users.noreply.github.com>
2021-06-01 18:12:26 +0530

    Add files via upload

---
+++ clerk.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library;
+
+
+public class Clerk extends Staff {
+
+    Clerk(int user_id, String user_name, char gender) {
+        super(user_id, user_name, gender);
+    }
+}
